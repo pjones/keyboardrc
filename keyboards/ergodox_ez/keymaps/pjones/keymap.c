bee3b5730046f5be55199aa27c82d04680e92c92
Peter Jones <pjones@devalot.com>
2017-10-22 13:16:57 -0700

    Fix font and volume keys

--- keyboards/ergodox_ez/keymaps/pjones/keymap.c
+++ keyboards/ergodox_ez/keymaps/pjones/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | ESC    |   1  |   2  |   3  |   4  |   5  | VUP  |           | VDN  |   6  |   7  |   8  |   9  |   0  |   CAPS |
+ * | ESC    |   1  |   2  |   3  |   4  |   5  | VDN  |           | VUP  |   6  |   7  |   8  |   9  |   0  |   CAPS |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |   Q  |   W  |   E  |   R  |   T  | Next |           | Play |   Y  |   U  |   I  |   O  |   P  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -44,33 +44,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        ,-------------.       ,-------------.
  *                                        |Super | L2   |       | L3   | Super|
  *                                 ,------|------|------|       |------+--------+------.
- *                                 | Back |      |      |       | F+   |        |      |
+ *                                 | Back |      | F+   |       |      |        |      |
  *                                 | Spc  | Ctrl |------|       |------|  Alt   | Space|
- *                                 |      |      |      |       | F-   |        |      |
+ *                                 |      |      | F-   |       |      |        |      |
  *                                 `--------------------'       `----------------------'
  */
 [L_BASE] = LAYOUT_ergodox(
         // left hand
-        KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_VOLD,
+        KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_VOLU,
         KC_NO,          KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_MNXT,
         SFT_T(KC_TAB),  KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_NO,          KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   OSL(L_SYMB),
         KC_NO,          KC_NO,        KC_NO,  KC_NO,  M(M_CALC),
 
                                     OSM(MOD_LGUI), OSL(L_MOVEMENT),  
-                                                                 KC_NO,
-                                               KC_BSPC,KC_LCTRL, KC_NO,
+                                                                 FONT_PLUS,
+                                               KC_BSPC,KC_LCTRL, FONT_MINUS,
 
         // right hand
-             KC_VOLD,      KC_6,   KC_7,        KC_8,     KC_9,   KC_0,     KC_CAPS,
+             KC_VOLU,      KC_6,   KC_7,        KC_8,     KC_9,   KC_0,     KC_CAPS,
              KC_MPLY,      KC_Y,   KC_U,        KC_I,     KC_O,   KC_P,     KC_NO,
                            KC_H,   KC_J,        KC_K,     KC_L,   KC_SCLN,  SFT_T(KC_ENT),
           OSL(L_SYMB), KC_N,   KC_M,        KC_COMM,  KC_DOT, KC_SLSH,  KC_NO,
                                    M(M_PASSWD), KC_NO,    KC_NO,  KC_NO,    KC_NO,
 
              OSL(L_DRAWING), OSM(MOD_LGUI),
-             FONT_PLUS,
-             FONT_MINUS, KC_RALT, KC_SPC
+             KC_NO,
+             KC_NO, KC_RALT, KC_SPC
 ),
 /* Keymap 1: Symbol Layer
  *
