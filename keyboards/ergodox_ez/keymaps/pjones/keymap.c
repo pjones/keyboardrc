3bfc5571d97d01cbfc2a81dbb5549eba578f33f5
Peter Jones <pjones@devalot.com>
2017-12-16 16:25:44 -0700

    Move modifiers around a lot to make them more comfortable

--- keyboards/ergodox_ez/keymaps/pjones/keymap.c
+++ keyboards/ergodox_ez/keymaps/pjones/keymap.c
@@ -29,7 +29,8 @@ enum {
 
 /******************************************************************************/
 enum custom_keycodes {
-  PLACEHOLDER = SAFE_RANGE, // can always be here
+  K_SYMB = SAFE_RANGE, // can always be here
+  K_NUMB
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -38,46 +39,46 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | ESC    |   1  |   2  |   3  |   4  |   5  |S-s h |           | S-s l|   6  |   7  |   8  |   9  |   0  |   CAPS |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Super  |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |  Super |
+ * |    "   |   Q  |   W  |   E  |   R  |   T  | Play |           | Next |   Y  |   U  |   I  |   O  |   P  |    '   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | Tab/Sf |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   | Ent/Sf |
- * |--------+------+------+------+------+------| Symb |           | Symb |------+------+------+------+------+--------|
- * | Numbers|   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   |Numbers |
+ * |Tab/Sft |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   | Ent/Sft|
+ * |--------+------+------+------+------+------| Super|           |Super |------+------+------+------+------+--------|
+ * |    (   |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   |    )   |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      | CALC |                                       | PASS |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |C-x b | MOVE |       | DRAW | C-c r|
+ *                                        |C-x b |      |       | DRAW | C-c r|
  *                                 ,------|------|------|       |------+--------+------.
- *                                 | Back |      | F+   |       | PgUp |        |      |
- *                                 | Spc  | Ctrl |------|       |------|  Alt   | Space|
- *                                 |      |      | F-   |       | PgDn |        |      |
+ *                                 | Bspc |      | F+   |       | PgUp |        | Spc  |
+ *                                 |      | Syms |------|       |------|  Nums  |      |
+ *                                 | Ctrl |      | F-   |       | PgDn |        | Alt  |
  *                                 `--------------------'       `----------------------'
  */
 [L_BASE] = LAYOUT_ergodox(
   // Left Hand:
   KC_ESC,         KC_1,  KC_2,   KC_3,   KC_4,      KC_5,    M(M_SCREEN_PREV),
-  OSM(MOD_LGUI),  KC_Q,  KC_W,   KC_E,   KC_R,      KC_T,    KC_MPLY,
+  KC_DQUO,        KC_Q,  KC_W,   KC_E,   KC_R,      KC_T,    KC_MPLY,
   SFT_T(KC_TAB),  KC_A,  KC_S,   KC_D,   KC_F,      KC_G,    /* 2U */
-  TT(L_NUMBERS),  KC_Z,  KC_X,   KC_C,   KC_V,      KC_B,    OSL(L_SYMB),
+  KC_LPRN,        KC_Z,  KC_X,   KC_C,   KC_V,      KC_B,    OSM(MOD_LGUI),
   KC_NO,          KC_NO, KC_NO,  KC_NO,  M(M_CALC), /* NA */ /* NA */
 
   // Left Thumb Cluster:
-  /* NA */ M(M_CXB),  TT(L_MOVEMENT),
-  /* 2U */ /* 2U */   FONT_PLUS,
-  KC_BSPC, KC_LCTRL,  FONT_MINUS,
+  /* NA */        M(M_CXB),   TT(L_MOVEMENT),
+  /* 2U */        /* 2U */    FONT_PLUS,
+  CTL_T(KC_BSPC), K_SYMB, FONT_MINUS,
 
   // Right Hand:
   M(M_SCREEN_NEXT), KC_6,      KC_7,        KC_8,     KC_9,   KC_0,     KC_CAPS,
-  KC_MNXT,          KC_Y,      KC_U,        KC_I,     KC_O,   KC_P,     OSM(MOD_LGUI),
+  KC_MNXT,          KC_Y,      KC_U,        KC_I,     KC_O,   KC_P,     KC_QUOT,
   /* 2U */          KC_H,      KC_J,        KC_K,     KC_L,   KC_SCLN,  SFT_T(KC_ENT),
-  OSL(L_SYMB),      KC_N,      KC_M,        KC_COMM,  KC_DOT, KC_SLSH,  TT(L_NUMBERS),
+  OSM(MOD_RGUI),    KC_N,      KC_M,        KC_COMM,  KC_DOT, KC_SLSH,  KC_RPRN,
   /* NA */          /* NA */   M(M_PASSWD), KC_NO,    KC_NO,  KC_NO,    KC_NO,
 
   // Right Thumb Cluster:
-  TT(L_DRAWING), M(M_CCR),  /* NA */
-  KC_PGUP,       /* 2U */   /* 2U */
-  KC_PGDN,       KC_LALT,   KC_SPC
+  TT(L_DRAWING), M(M_CCR), /* NA */
+  KC_PGUP,       /* 2U */  /* 2U */
+  KC_PGDN,       K_NUMB,   ALT_T(KC_SPC)
 ),
 
 /* Numbers Layer
@@ -132,13 +133,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,---------------------------------------------------.           ,--------------------------------------------------.
  * | RESET   |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * |    ~    |   \  |   `  |   |  |   +  |   =  |      |           |      |   _  |   -  |   ~  |   [  |   ]  |   F12  |
+ * |         |   \  |   `  |   |  |   +  |   =  |      |           |      |   _  |   -  |   ~  |   [  |   ]  |   F12  |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |    "    |   !  |   @  |   #  |   $  |   %  |------|           |------|   ^  |   &  |   *  |   (  |   )  |        |
- * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |      |      |      |      |      |      |           |      |      |      |      |   {  |   }  |        |
+ * |    "    |   !  |   @  |   #  |   $  |   %  |------|           |------|   ^  |   &  |   *  |   {  |   }  |   '    |
+ * |---------+------+------+------+------+------|  "   |           |  '   |------+------+------+------+------+--------|
+ * |         |      |      |      |      |      |      |           |      |      |      |      |   <  |   >  |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |       |      |      |      |      |                                       |      |      |   <  |  >   |      |
+ *   |       |      |      |      |      |                                       |      |      |      |      |      |
  *   `-----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
@@ -151,9 +152,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [L_SYMB] = LAYOUT_ergodox(
   // Left Hand:
   RESET,   KC_F1,      KC_F2,    KC_F3,    KC_F4,    KC_F5,      KC_TRNS,
-  KC_TILD, KC_BSLASH,  KC_GRAVE, KC_PIPE,  KC_PLUS,  KC_EQUAL,   KC_NO,
+  KC_TRNS, KC_BSLASH,  KC_GRAVE, KC_PIPE,  KC_PLUS,  KC_EQUAL,   KC_NO,
   KC_DQUO, KC_EXLM,    KC_AT,    KC_HASH,  KC_DLR,   KC_PERCENT, /* 2U */
-  KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,
+  KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_DQUO,
   KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  /* NA */  /* NA */
 
   // Left Thumb Cluster:
@@ -164,9 +165,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Right Hand:
   KC_TRNS, KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,
   KC_NO,   KC_UNDS, KC_MINUS, KC_TILD, KC_LBRC, KC_RBRC, KC_F12,
-  /* 2U */ KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, KC_QUOT,
-  KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_LCBR, KC_RCBR, KC_TRNS,
-  /* NA */ /* NA */ KC_TRNS,  KC_TRNS, KC_LABK, KC_RABK, KC_TRNS,
+  /* 2U */ KC_CIRC, KC_AMPR,  KC_ASTR, KC_LCBR, KC_RCBR, KC_QUOT,
+  KC_QUOT, KC_TRNS, KC_TRNS,  KC_TRNS, KC_LABK, KC_RABK, KC_TRNS,
+  /* NA */ /* NA */ KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 
   // Right Thumb Cluster:
   KC_TRNS, KC_TRNS, /* NA */
@@ -292,6 +293,29 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+  case K_SYMB:
+    if (record->event.pressed) {
+      layer_on(L_SYMB);
+      update_tri_layer(L_SYMB, L_NUMBERS, L_MOVEMENT);
+    } else {
+      layer_off(L_SYMB);
+      update_tri_layer(L_SYMB, L_NUMBERS, L_MOVEMENT);
+    }
+    return false;
+    break;
+  case K_NUMB:
+    if (record->event.pressed) {
+      layer_on(L_NUMBERS);
+      update_tri_layer(L_SYMB, L_NUMBERS, L_MOVEMENT);
+    } else {
+      layer_off(L_NUMBERS);
+      update_tri_layer(L_SYMB, L_NUMBERS, L_MOVEMENT);
+    }
+    return false;
+    break;    
+  }
+  
   return true;
 }
 
